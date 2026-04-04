7955268e8d98b995c54e64f74565abb92117d449
fs1g17 <fs1g17@soton.ac.uk>
2026-04-04 19:10:09 +0400

    feat: use coder/websocket instead

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/fs1g17/ws-chat
 
 go 1.24.5
 
-require github.com/gorilla/websocket v1.5.3 // indirect
+require (
+	github.com/coder/websocket v1.8.14 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,40 +1,59 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"
+	"github.com/coder/websocket"
 )
 
-var upgrader = websocket.Upgrader{}
-
-func echo(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
+func serve(w http.ResponseWriter, r *http.Request) {
+	c, err := websocket.Accept(w, r, nil)
 	if err != nil {
-		log.Println("read:", err)
+		log.Printf("%v", err)
 		return
 	}
-	defer c.Close()
+	defer c.CloseNow()
 
 	for {
-		mt, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			break
+		err = echo(c)
+		if websocket.CloseStatus(err) == websocket.StatusNormalClosure {
+			return
 		}
-		log.Printf("recv: %s", message)
-		err = c.WriteMessage(mt, message)
 		if err != nil {
-			log.Println("write:", err)
-			break
+			fmt.Printf("error: %v", err)
+			return
 		}
 	}
 }
 
+func echo(c *websocket.Conn) error {
+	ctx := context.Background()
+	typ, r, err := c.Reader(ctx)
+	if err != nil {
+		return err
+	}
+
+	w, err := c.Writer(ctx, typ)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(w, r)
+	if err != nil {
+		return fmt.Errorf("failed to io.Copy: %w", err)
+	}
+
+	err = w.Close()
+	return err
+}
+
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.HandleFunc("/api/echo", echo)
+	http.HandleFunc("/api/echo", serve)
 
 	http.ListenAndServe(":8080", nil)
 }
